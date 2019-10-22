c206d93f2300899a93a0492baca0bc9dc00b6c7f
Ash (She) <aadamson@hashicorp.com>
2019-10-22 15:29:17 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,13 +3,13 @@ variable "environment" {
     description = "The name of the environment in which you would like to provision"
 }
 
-resource "random_pet" "host" {
-    keepers = {
-    # Generate a new id each time the system time changes
-    timestamp = "${timestamp()}"
-  }
-}
+# resource "random_pet" "host" {
+#     keepers = {
+#     # Generate a new id each time the system time changes
+#     timestamp = "${timestamp()}"
+#   }
+# }
 
-output "host" {
-  value = "${random_pet.host.id}-${lower(var.environment)}"
-}
+# output "host" {
+#   value = "${random_pet.host.id}-${lower(var.environment)}"
+# }
